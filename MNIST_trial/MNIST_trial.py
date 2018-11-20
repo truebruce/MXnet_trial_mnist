b8b4f2e57fe9cb376e02317eb52a33ff07f375ff
truebruce <truebruce@sina.com>
2018-11-21 00:46:16 +0800

    Update: Change pooling from 'max' to 'avg'. Validation-accuracy=0.994309. Train=0.998700, overfitted.

--- MNIST_trial/MNIST_trial.py
+++ MNIST_trial/MNIST_trial.py
@@ -81,12 +81,12 @@ data = mx.symbol.Variable('data')
 # 32 neurons of Conv. filter layer with BN layer: 5*5/1 filter, 3*3/2*2 max pooling
 net = GenerateConvolutionalLayers(data, (3,3), 32, 'relu', 'Conv_1')
 net = GenerateConvolutionalLayers(net, (3,3), 32, 'relu', 'Conv_1_2')
-net = mx.sym.Pooling(net, (3,3), "max", stride=(2,2), name="Conv_1_pooling")
+net = mx.sym.Pooling(net, (3,3), "avg", stride=(2,2), name="Conv_1_pooling")
 
 # 64 neurons of Conv. filter layer with BN layer: 5*5/1 filter, 3*3/2*2 max pooling
 net = GenerateConvolutionalLayers(net, (3,3), 16, 'relu', 'Conv_2')
 net = GenerateConvolutionalLayers(net, (3,3), 16, 'relu', 'Conv_2_2')
-net = mx.sym.Pooling(net, (3,3), "max", stride=(2,2), name="Conv_2_pooling")
+net = mx.sym.Pooling(net, (3,3), "avg", stride=(2,2), name="Conv_2_pooling")
 
 # 10 neurons of Conv. filter layer: 3*3/1 filter, 1*1 average global pooling
 net = mx.sym.Convolution(net, name="Conv_3", kernel=(3,3), num_filter=10)
